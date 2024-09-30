977940ea36afc204e44eecb25be08659fd572c1b
KJRMalshan <ravindumalshan1111@gmail.com>
2024-10-01 00:18:22 +0530

    new java

---
+++ Display.java
@@ -0,0 +1,15 @@
+// Write a Java application to display the following numeric values on the screen. 
+public class Display{
+	public static void main(String[] args){
+		//a
+		System.out.println(345);
+		//b
+		System.out.println(12.5);
+		//c
+		System.out.println(-0.00045);
+		//d
+		System.out.println(12.3E4);
+		//e
+		System.out.println(0.03e-05);
+	}
+}
