4a126529a00b76c58f6fa70c25b24c804b30d90f
KJRMalshan <ravindumalshan1111@gmail.com>
2024-10-03 01:27:24 +0530

    Prctice 2

---
+++ Massage.java
@@ -0,0 +1,9 @@
+//3. Write a java application to Display following messages. 
+public class Massage{
+	public static void main(String[] args){
+		System.out.println(" Today's temperature is 36\u00B0 C." );
+		System.out.println(" I have \u00A3 30 ");
+		System.out.println("\u03B2 - Testing in progress");
+		System.out.println("Area = \u03C0r^2");
+	}
+}
