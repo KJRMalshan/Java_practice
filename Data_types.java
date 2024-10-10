1f91dea95c805b11cf1743972f5164e750fd70b7
KJRMalshan <ravindumalshan1111@gmail.com>
2024-10-10 23:58:43 +0530

    Practice

---
+++ Data_types.java
@@ -0,0 +1,13 @@
+class Data_types{
+	public static void main(String[] args){
+		int age = 23;
+		double height = 5.7;
+		char initial = 'K';
+		boolean student = true;
+		
+		System.out.println("My age is "+ age);
+		System.out.println("My height is "+ height);
+		System.out.println("My first initial is "+ initial);
+		System.out.println("Is student :"+ student);
+	}
+}
